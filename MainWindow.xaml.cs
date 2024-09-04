5d424fb62602ea73393321ccf4c2e206e90cae45
Oleh Radchenko <89965788+OlehRadchenko@users.noreply.github.com>
2024-09-04 11:11:04 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INF04_Styczen_2024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Numer_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string numer = Numer.Text;
+            Imie.Text = numer;
+            /*Zdjecie.Source = new BitmapImage(new Uri(@"000-zdjecie.png", UriKind.Relative));
+            Odcisk.Source = new BitmapImage(new Uri(@"000-odcisk.png", UriKind.Relative));*/
+        }
+    }
+}
